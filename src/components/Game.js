1b3a8de9b772af5d8c91f3c70fb3d5976c0602b4
ivanThePleasant <sevcenko.ivan@gmail.com>
2021-12-22 23:44:42 +0200

    Keypress game move fucntionality addeed

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Steps from "./Steps";
 
@@ -8,57 +8,79 @@ import bg from '../assets/bg.gif'
 const fixes = [
   {
     name: "Typo",
+    icon: ["fas", "font"],
   },
   {
     name: "Connector",
+    icon: ["fas", "plug"],
   },
   {
     name: "Fix bad PR",
+    icon: ["fas", "code-branch"],
   },
   {
     name: "Missing dependencies",
+    icon: ["fas", "link"],
   },
   {
     name: "Documentation",
+    icon: ["fas", "file-alt"],
   },
-];
-
-const replies = [
+  {
+    name: "Push code",
+    icon: ["fas", "upload"],
+  },
   {
     name: "Node version",
+    icon: ["fab", "node-js"],
   },
   {
     name: "Check config",
+    icon: ["fas", "cog"],
   },
   {
-    name: "Documentation",
+    name: "Ask community",
+    icon: ["fas", "comments"],
   },
   {
     name: "It's fixed in V4",
+    icon: ["fas", "hammer"],
   },
   {
     name: "Learn NGINX",
+    icon: ["fas", "server"],
+  },
+  {
+    name: "Comment",
+    icon: ["fas", "comment"],
   },
 ];
 
+
 const getNewIssue = () => {
   const issue = []
-  const numberOfSteps = Math.floor(Math.random() * 4)
+  let numberOfSteps = Math.floor(Math.random() * 4)
   const issueType = Math.floor(Math.random() * 2)
 
   const assembleTicket = (issueType) => {
     for (let i = 0; i < numberOfSteps; i++) {
-      const randomStep = issueType[Math.floor(Math.random() * issueType.length)];
+      const randomStep = issueType[Math.floor(Math.random() * (issueType.length))];
+      if (randomStep.name === 'Comment' || randomStep.name === 'Push code') {
+        numberOfSteps += 1
+        break
+      }
       issue.push({
         ...randomStep,
       });
     }
   }
 
+  assembleTicket(fixes);
+
   if (issueType === 0) {
-    assembleTicket(fixes)
+    issue.push(fixes[5])
   } else {
-    assembleTicket(replies)
+    issue.push(fixes[11]);
   }
 
   return issue
@@ -66,10 +88,58 @@ const getNewIssue = () => {
 }
 
 const Game = () => {
+  const [selectedStep, setSelectedStep] = useState(-1)
+  const [currentSteps, setCurrentSteps] = useState([]);
+
+
+
+  useEffect(() => {
+    const listener = (event) => {
+      
+      
+        if (event.key === "ArrowRight") {
+          setSelectedStep((prevValue) => {
+            const nextIndex = prevValue + 1;
+            return nextIndex >= fixes.length ? 0 : nextIndex;
+          });
+        } else if (event.key === "ArrowLeft") {
+          setSelectedStep((prevValue) => {
+            const nextIndex = prevValue - 1;
+            return nextIndex < 0 ? fixes.length - 1 : nextIndex;
+          });
+        } else if (event.key === "ArrowUp") {
+          setSelectedStep((prevValue) => {
+            const nextIndex = prevValue - 6;
+            return nextIndex < 0 ? prevValue + 6  : nextIndex;
+          });
+        } else if (event.key === "ArrowDown") {
+          setSelectedStep((prevValue) => {
+            const nextIndex = prevValue + 6;
+            return nextIndex >= fixes.length ? prevValue - 6 : nextIndex;
+          });
+        } else if (event.key === "Enter") {
+          setCurrentSteps((prevValue) => [
+            ...prevValue,
+            {
+              id: Date.now(),
+              ...fixes[selectedStep],
+            },
+          ]);
+        }
+      
+    };
+    
+    document.addEventListener("keydown", listener);
+    
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, []);
+
   return (
     <div className={styles.game}>
       <img className={styles.background } src={bg} alt="background"/>
-      <Steps />
+      <Steps steps={fixes} selectedStep={selectedStep} />
     </div>
   )
 }
--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,25 +1,42 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab, faNodeJs } from "@fortawesome/free-brands-svg-icons";
+import { 
+  fas, faFont, faPlug, 
+  faCodeBranch, faLink, 
+  faFileAlt, faCog, faHammer, 
+  faServer, faUpload, faComment,
+  faComments } from "@fortawesome/free-solid-svg-icons";
+
 
 import styles from './Steps.module.css'
 
+library.add(
+  fab, fas, faFont, 
+  faPlug, faCodeBranch, 
+  faLink, faFileAlt, 
+  faNodeJs, faCog, faHammer, 
+  faServer, faUpload, faComment,
+  faComments)
+
 const classNames = (...names) => names.join(' ')
 
-const Steps = () => {
+const Steps = ({ steps, selectedStep }) => {
   return (
     <div className={classNames(styles.container)}>
       <div className={classNames(styles.steps)}>
-        <div className={styles.step}></div>
-        <div className={styles.step}></div>
-        <div className={styles.step}></div>
-        <div className={styles.step}></div>
-        <div className={styles.step}></div>
-        <div className={styles.step}></div>
-        <div className={styles.step}></div>
-        <div className={styles.step}></div>
-        <div className={styles.step}></div>
-        <div className={styles.step}></div>
-        <div className={styles.step}></div>
-        <div className={styles.step}></div>
+        {steps.map((step, index) => (
+          <div
+            key={step.name}
+            className={classNames(
+              styles.step,
+              selectedStep === index ? styles.selected : ""
+            )}
+          >
+            <FontAwesomeIcon icon={step.icon} size="2x" />
+          </div>
+        ))}
       </div>
     </div>
   );
